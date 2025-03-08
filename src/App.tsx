6fe28b8fbd74e774a1bb789018b8024a3829a6c2
Peter <hello.ship.code@gmail.com>
2025-03-08 23:03:20 +0530

    props 🚣🏻‍♂️

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { CardComponent } from "./components/cardComponent/card.component"
+// import { CardComponent } from "./components/cardComponent/card.component"
 // import { FoodsComponent } from "./components/food.component"
 import { FooterComponent } from "./components/footer.component"
 import { HeaderComponent } from "./components/header.component"
+import { UserGreetingComponent } from "./components/userGreeting.component"
 
 export const App = () => {
 
   return (
     <>
       < HeaderComponent />
+      <UserGreetingComponent isLoggedIn={true} userName="Peter" />
+      <UserGreetingComponent isLoggedIn={false} userName="Pranay" />
       {/* < FoodsComponent /> */}
-      < CardComponent />
+      {/* < CardComponent /> */}
       < FooterComponent />
 
     </>
--- src/components/buttons/basic.button.component.tsx
+++ src/components/buttons/basic.button.component.tsx
@@ -1,15 +1,70 @@
-import "./basic.button.component.css"
+import styles from "./basic.button.component.module.css"
 
 type ButtonProps = {
   text: string,
   alertMessage: string
 }
-export const BasicButtonComponent = ({ text, alertMessage }: ButtonProps) => {
+
+export const BasicButtonComponent = (props: ButtonProps) => {
   return (
     <>
-      <button className="button" onClick={() => alert(alertMessage)}>
-        {text}
+      <button className={styles.button} onClick={() => alert(props.alertMessage)}>
+        {props.text}
       </button>
     </>
   )
 }
+// should install propTypes package waste of time. We are using TS
+
+// BasicButtonComponent.propTypes = {
+//   text: propTypes.string,
+//   alertMessage: propTypes.string,
+//   age: propTypes.number,
+//   isEmployed: propTypes.bool,
+// }
+
+// inline styles
+
+{/* <button
+style={{
+  backgroundColor: "blue",
+  color: "white",
+  padding: "10px 20px",
+  border: "none",
+  cursor: "pointer",
+  borderRadius: "5px",
+  fontSize: "16px",
+}}
+onClick={() => alert(alertMessage)}
+>
+{text}
+</button> */}
+
+// tstyles
+
+// type ButtonProps = {
+//   text: string;
+//   alertMessage: string;
+// };
+
+// const tstyles = {
+//   button: {
+//     backgroundColor: "blue",
+//     color: "white",
+//     padding: "10px 20px",
+//     border: "none",
+//     cursor: "pointer",
+//     borderRadius: "5px",
+//     fontSize: "16px",
+//   },
+// };
+
+// export const BasicButtonComponent = ({ text, alertMessage }: ButtonProps) => {
+//   return (
+//     <>
+//       <button style={tstyles.button} onClick={() => alert(alertMessage)}>
+//         {text}
+//       </button>
+//     </>
+//   );
+// };
